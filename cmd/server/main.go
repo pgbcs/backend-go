329611d182ff080de99e844ea37f584887e89de4
pgbao <pbao2406@gmail.com>
2026-02-15 17:12:33 +0700

    SCRUM-32 Initialize Go module and add dependencies

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env not found, using OS environment")
+	}
+
+	r := gin.Default()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":" + port)
+}
